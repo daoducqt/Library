a9335a7b93ed2dfeb8af4e5f248f74dd5174e7e3
Orendal3001 <tranbatoan30012003@gmail.com>
2026-01-02 12:03:34 +0700

    fix category chưa fix xong fine

--- library-backend/src/fine/model/fine.js
+++ library-backend/src/fine/model/fine.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const FineSchema = new mongoose.Schema(
     {
         userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        loanId: { type: mongoose.Schema.Types.ObjectId, ref: "LoanDetail", required: true },
+        loanId: { type: mongoose.Schema.Types.ObjectId, ref: "Loan", required: true },
         daysLate: { type: Number, required: true, min: 1},
         amount: { type: Number, required: true, min: 0},
         isPayed: { type: Boolean, default: false, index: true },
